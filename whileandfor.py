6e26ee7d97ce1724677eca2d881186de69168880
Xirdman <Xirdman@mail.ru>
2020-04-10 23:12:41 +0400

    2

---
+++ whileandfor.py
@@ -0,0 +1,19 @@
+i = 5
+while i< 10:
+    print(i)
+    i=i+1
+
+for j in "String to double":
+    print(j*2)
+
+for z in "Hello" :
+    if z == 'e' :
+        continue
+    print(z)
+
+for v in "Ещё строка":
+    if v == 'м':
+        print("буква м в строке есть")
+        break
+else : print("Буквы м в строке нет")
+
